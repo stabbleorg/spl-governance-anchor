06c67d7b581a9a7916904df1889efd9ea1992586
stabble-devops <keisuke@stabble.org>
2025-06-17 05:38:39 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+#![allow(unused)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw");
+
+#[program]
+pub mod spl_governance {
+    use super::*;
+
+    pub fn deposit_governing_tokens(ctx: Context<DepositGoverningTokens>, amount: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn withdraw_governing_tokens(ctx: Context<WithdrawGoverningTokens>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct DepositGoverningTokens<'info> {
+    pub realm: AccountInfo<'info>,
+    #[account(mut)]
+    pub governing_token_holding: AccountInfo<'info>,
+    #[account(mut)]
+    pub governing_token_source: AccountInfo<'info>,
+    #[account(mut)]
+    pub governing_token_owner: AccountInfo<'info>,
+    pub governing_token_source_authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub token_owner_record: AccountInfo<'info>,
+    #[account(mut)]
+    pub payer: AccountInfo<'info>,
+    pub system_program: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+    #[account(mut)]
+    pub realm_config: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawGoverningTokens<'info> {
+    pub realm: AccountInfo<'info>,
+    #[account(mut)]
+    pub governing_token_holding: AccountInfo<'info>,
+    #[account(mut)]
+    pub governing_token_destination: AccountInfo<'info>,
+    #[account(mut)]
+    pub governing_token_owner: AccountInfo<'info>,
+    #[account(mut)]
+    pub token_owner_record: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+    #[account(mut)]
+    pub realm_config: AccountInfo<'info>,
+}
